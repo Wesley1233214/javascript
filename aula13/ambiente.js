b0d44450aad7e40c550acc0e044270911f3f10b9
Wesley Gabriel da Silva Ribiro <wesleyz123gabrielribeiro@gmail.com>
2023-02-15 16:24:34 +0000

    variavel loop

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,14 @@
+var c = 1 
+do { /* Estrutura de repetiçao no final da estrutura*/
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+ /*
 var c = 1
-while (c <= 999) {
+while (c <= 6) { -> (Estrutura de repetição no começo da estrutura)
     console.log(`Passo ${c}`)
     c++
 }
+*/
--- aula13/node_5cc29780c77b3.tmp.js
+++
@@ -1,5 +0,0 @@
-var c = 1
-while (c <= 9999999999) {
-    console.log(`Passo ${c}`)
-    c++
-}
